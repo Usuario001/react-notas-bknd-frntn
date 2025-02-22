455c61381b15cc1a1b980b73162dcd1bb8a78040
usuario001 <patinogutierrez@icloud.com>
2025-02-21 23:57:03 -0600

    file upload endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,71 @@
-import express from 'express';
+import express, { raw } from 'express';
 import cors from 'cors'
+import multer from 'multer'
+import csvToJson from 'convert-csv-to-json'
 
 const app = express()
-
 const port = process.env.PORT ?? 3000
+
+const storage = multer.memoryStorage()
+const upload = multer({storage})
+
+let userData: Array<Record<string, string>> = []
 
 app.use(cors())
 
-app.post('/api/files', (req, res) => {
-    res.status(200)
+app.post('/api/files', upload.single('file'), async (req, res) => {
+    //1) Extract file from request
+    const { file } = req
+    //2) Validate that we have the file
+    if(!file){
+        res.status(500).json({message:'File is required'})
+        return;
+    }
+    //3) Validate the mimetype is only CSV 
+    if(file.mimetype !== 'text/csv'){
+        res.status(500).json({message:'File must be a CSV type'})
+        return;
+    }
+    let json: Array<Record<string,string>>
+    try{
+        //4) Transform CSV to string (with buffer)
+        const rawCsv = Buffer.from(file.buffer).toString('utf-8')
+        //5) Transform string to json
+        json = csvToJson.fieldDelimiter(',').csvStringToJson(rawCsv)
+    }catch(error){
+        res.status(500).json({message:'Error parsing the file'})
+        return;
+    }
+    //save json to DB (not yet just in memory xD)
+    userData = json
+    //return 200 with the message and the json 
+    res.status(200).json({data:json, message:'File was upload succesfully'})
+    return;
 })
 
-app.get('/api/users', (req, res) => {
+
+//TODO finish this endpoint
+app.get('/api/users', async (req, res) => {
+    //extract the query param from request
+    const { q } = req.query
+    //validate that we have the query
+    if ( !q ){
+        res.status(500).json({message:'Query param `q` is required'})
+        return;
+    }
+
+    if( Array.isArray(q) ){
+        res.status(500).json({message:'Query q must be a string'})
+        return;
+    }
+
+    //Filter the data from the DB (Not yet xD) or the memory with the query param 
+
+    //Return 200 with the filtered data
+
     res.status(200).json({ data: [] })
-})
 
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript Express!');
-  });
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
